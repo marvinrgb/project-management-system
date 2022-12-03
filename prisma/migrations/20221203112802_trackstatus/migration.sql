892260024eeeb2e6074e79c6e7e6e4f58887c36f
marvin <marvinwolfart2@gmail.com>
2022-12-03 12:28:49 +0100

    status in database

---
+++ prisma/migrations/20221203112802_trackstatus/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Track" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT 'default',
+    "text" TEXT NOT NULL DEFAULT 'default',
+    "explicit" BOOLEAN NOT NULL DEFAULT false,
+    "length" INTEGER NOT NULL DEFAULT 0,
+    "genre" TEXT NOT NULL DEFAULT 'default',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" INTEGER NOT NULL DEFAULT 1,
+    "projectId" INTEGER NOT NULL,
+    CONSTRAINT "Track_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Track" ("createdAt", "description", "explicit", "genre", "id", "length", "name", "projectId", "text") SELECT "createdAt", "description", "explicit", "genre", "id", "length", "name", "projectId", "text" FROM "Track";
+DROP TABLE "Track";
+ALTER TABLE "new_Track" RENAME TO "Track";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
